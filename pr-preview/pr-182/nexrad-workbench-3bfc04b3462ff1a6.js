c0568ef7db6a8127717b64fb5e3b7221e9f43586
danielway <danielway@users.noreply.github.com>
2026-08-15 16:54:18 +0000

    Deploy preview for PR 182 🛫

--- pr-preview/pr-182/nexrad-workbench-3bfc04b3462ff1a6.js
+++ pr-preview/pr-182/nexrad-workbench-3bfc04b3462ff1a6.js
@@ -2224,7 +2224,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_000000000000000a: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [], shim_idx: 239, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [], shim_idx: 258, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen_d10ee817df58d99f___convert__closures_____invoke_______true_);
             return ret;
         },
